d9d51205f74ee2ecf1956e0fa7c842e67d6faed2
Vinicius <vinicius.arantes@compjunior.com.br>
2024-05-27 12:59:29 -0300

    Add index loaders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const app = require('./app');
+const Loaders = require('./loaders');
 require('dotenv').config();
 
 const port = process.env.PORT;
+
+Loaders.start();
 
 app.listen(port, () => console.log(`Server is on at port ${port}`));
